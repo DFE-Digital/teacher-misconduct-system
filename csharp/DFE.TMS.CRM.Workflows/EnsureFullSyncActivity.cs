72b1d7410e8d0582a8802def396978dd6ef3f278
ALEXANDER, Dane <Dane.ALEXANDER@EDUCATION.GOV.UK>
2022-05-16 18:54:22 +0100

    wip: added workflow assembly

---
+++ csharp/DFE.TMS.CRM.Workflows/EnsureFullSyncActivity.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+
+namespace DFE.TMS.CRM.Workflows
+{
+    public class EnsureFullSyncActivity : CodeActivity
+    {
+
+        //Define the properties
+        [Input("Bookable Resource")]
+        [ReferenceTarget("bookableresource")]
+        public InArgument<EntityReference> BookableResource { get; set; }
+
+        [Input("StartFrom")]
+        [Default("true")]
+        public InArgument<DateTime> StartFrom { get; set; }
+
+        [Input("EndTo")]
+        public InArgument<DateTime> EndTo { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            IWorkflowContext workflowContext = context.GetExtension<IWorkflowContext>();
+
+            //Create an Organization Service
+            IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(workflowContext.InitiatingUserId);
+
+
+        }
+    }
+}
