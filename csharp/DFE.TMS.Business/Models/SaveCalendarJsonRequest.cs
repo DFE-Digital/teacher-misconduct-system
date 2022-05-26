b9e442c0b6be10a05edf1e765211cf1659d1cd9e
ALEXANDER, Dane <Dane.ALEXANDER@EDUCATION.GOV.UK>
2022-05-26 18:46:21 +0100

    milestone: deactivated plugin and calling code within portal time off block

---
+++ csharp/DFE.TMS.Business/Models/SaveCalendarJsonRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFE.TMS.Business.Models
+{
+
+    public class SaveCalendarJsonRequest
+    {
+        public Calendareventinfo CalendarEventInfo { get; set; }
+    }
+
+    public class Calendareventinfo
+    {
+        public string CalendarId { get; set; }
+        public string EntityLogicalName { get; set; }
+        public int TimeZoneCode { get; set; }
+        public Rulesandrecurrence[] RulesAndRecurrences { get; set; }
+    }
+
+    public class Rulesandrecurrence
+    {
+        public Rule[] Rules { get; set; }
+    }
+
+    public class Rule
+    {
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public int Effort { get; set; }
+        public int WorkHourType { get; set; }
+    }
+
+}
